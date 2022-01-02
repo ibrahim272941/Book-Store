e9d578484f9cbe4ec6359167a09265356c01ab14
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2022-01-02 23:59:28 +0100

    new commit

--- src/BooksContext.js
+++ src/BooksContext.js
@@ -12,11 +12,28 @@ const BooksContextProvider = ({ children }) => {
   });
   useEffect(() => {
     bookList();
+    searchBook();
   }, []);
+  const author = "agatha christie";
+  const searchUrl = `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${apiKey}`;
+  const searchBook = async () => {
+    const data = await axios.get(searchUrl);
+    const bookList = data.data.results.lists;
+    const list = bookList.map((book) => book.books);
+
+    const reduce = [list].map((book) => {
+      return [].concat.apply([], [...book]);
+    });
+    reduce.map((book) =>
+      // book.rank === state.bookList.title &&
+      setState({ ...state, bookList: book })
+    );
+  };
   const bookList = async () => {
     const data = await axios.get(baseUrl);
     const booksArr = data.data.results.books;
-    setState({ ...state, bookList: booksArr });
+
+    // setState({ ...state, bookList: booksArr });
   };
   const addCart = (book) => {
     setState({
@@ -30,10 +47,10 @@ const BooksContextProvider = ({ children }) => {
         : [...state.cart, { ...book, count: 1 }],
     });
   };
-  const removeFromCart = (id) =>
+  const removeFromCart = (rank) =>
     setState({
       ...state,
-      cart: state.cart.filter((cartItem) => cartItem.id !== id),
+      cart: state.cart.filter((cartItem) => cartItem.rank !== rank),
     });
   const increase = (id) => {
     setState({
